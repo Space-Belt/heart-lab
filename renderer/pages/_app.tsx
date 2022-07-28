57e108c7bac265a31b14e7c16a8383180985091e
Space-Belt <dngurwn@gmail.com>
2022-07-28 18:37:18 +0900

    [Init] 프로젝트 초기설정

--- renderer/pages/_app.tsx
+++ renderer/pages/_app.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
+import '../styles/global.css';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <React.Fragment>
-      <Head>
-        <meta name='viewport' content='width=device-width, initial-scale=1' />
-      </Head>
-      <Component {...pageProps} />
-    </React.Fragment>
-  );
+    return (
+        <React.Fragment>
+            <Head>
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1"
+                />
+            </Head>
+            <Component {...pageProps} />
+        </React.Fragment>
+    );
 }
 
 export default MyApp;
--- renderer/pages/next.tsx
+++ renderer/pages/next.tsx
@@ -1,27 +1,14 @@
-import React from 'react';
-import Head from 'next/head';
-import Link from 'next/link';
-import { BasicCard } from '../components/BasicCard';
-import { TitleCard } from '../components/TitleCard';
-import { bounce, AnimatedCard } from '../components/AnimatedCard';
+// import React from 'react';
+// import Head from 'next/head';
 
-function Next() {
-  return (
-    <React.Fragment>
-      <Head>
-        <title>Next - Nextron (with-typescript-emotion)</title>
-      </Head>
-      <div>
-        <TitleCard>Nextron with Emotion</TitleCard>
-        <BasicCard>
-          <Link href="/home">
-            <a>Go to home page</a>
-          </Link>
-        </BasicCard>
-        <AnimatedCard animation={bounce}>Let's bounce.</AnimatedCard>
-      </div>
-    </React.Fragment>
-  );
-};
+// function Next() {
+//     return (
+//         <React.Fragment>
+//             <Head>
+//                 <title>Next - Nextron (with-typescript-emotion)</title>
+//             </Head>
+//         </React.Fragment>
+//     );
+// }
 
-export default Next;
+// export default Next;
